278a1ea2b392a541d16cbbe3bb0afd9ea315fa29
ramanji4 <ramanji181818@gmail.com>
2025-07-04 13:41:09 +0530

    Eksctl

--- workstation.tf
+++ workstation.tf
@@ -6,7 +6,15 @@ module "workstation" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
   subnet_id              = var.public_subnet_id
-  user_data = file("workstation.sh") 
+  user_data = file("workstation.sh")
+
+  root_block_device = [
+    {
+      volume_size = 50          # Size in GiB
+      volume_type = "gp3"       # Recommended general purpose volume type
+      delete_on_termination = true
+    }
+  ]
 
   tags = {
     Name = "workstation"
